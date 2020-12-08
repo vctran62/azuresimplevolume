8f20db38459453f42f2da8925e4fff6211fe857a
Vinh Tran <vctran62@users.noreply.github.com>
2020-12-08 15:43:46 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
+}
